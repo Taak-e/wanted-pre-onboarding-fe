e212f9958e1f473fb101bcd7ce58756df9020b64
Taak-e <kjtag0408@gmail.com>
2022-08-23 17:57:31 +0900

    Commit. 07
    로그인 시 로컬스토리지에 토큰 저장기능 구현 완료
    
    2022.08.23
    Commit by Taak-e

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,40 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import {Grid, Text} from "../elements/index";
+import { signIn } from "../apis/auth";
 
 export default function Login (props) {
 
+       const [input, setInput] = useState({
+    userId: "",
+    userPwd: "",
+  });
+  const { userId, userPwd } = input;
+  
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const submit = async (e) => {
+    const data = {
+      email: userId,
+      password: userPwd,
+    };
+    e.preventDefault();
+    signIn(data).then((res) => {
+
+      console.log(res.data);
+      localStorage.setItem("JWT", res.data.access_token)
+    });
+  };
+
   return (
     <>
-      {/* <Grid padding="16px">
+      <Grid padding="16px">
         <Text size="32px" bold>
           로그인
         </Text>
 
         <Grid padding="16px 0px">
-          아이디
+          E-mail 
           <input
+            name="userId"
             type="text"
-            onChange={inputData}
-            placeholder="e-mail"
+            onChange={handleInput}
             value={userId}
           />
         </Grid>
 
         <Grid padding="16px 0px">
-          비밀번호
+          비밀번호  
           <input
+            name="userPwd"
             type="password"
-            onChange={inputData}
-            placeholder="e-mail"
+            onChange={handleInput}
             value={userPwd}
           />
         </Grid>
 
-        <Button>
-          가입하기
+
+        <Button 
+          onClick={submit}>
+          로그인하기
         </Button>
-      </Grid> */}
+      </Grid>
+
     </>
   );
 };
